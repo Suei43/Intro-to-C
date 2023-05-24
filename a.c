48bf19fb356f18f1f6b88aadd95bed231648fe9f
Suei43 <sueiraphael@gmail.com>
2023-05-25 00:48:56 +0100

    looping through an array

--- a.c
+++ a.c
@@ -10,4 +10,13 @@ int main(){
     double num = 10.34;
     int num2 = (int)num;
     printf("num: %d\n", num2);
+
+    //Print all elements in an array
+    int arr[5] = {1,2,3,4,5};
+    int i = 0;
+    printf("The size of the given array is: %d\n", sizeof(arr)/sizeof(arr[0]));
+    while(i<sizeof(arr)/sizeof(arr[0])){
+        printf("%d\n", arr[i]);
+        i++;
+    }
 }
