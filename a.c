1bdcf22887bc0b37e414b62b31d0ed36daf7d6f0
Suei43 <sueiraphael@gmail.com>
2023-05-24 23:12:44 +0100

    Print user's name

--- a.c
+++ a.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(){
-    printf("Hello Worlddddd!\n");
+    printf("Enter your name:");
     char name[20];
     scanf("%s", name);
     printf("Hello %s!\n", name);
