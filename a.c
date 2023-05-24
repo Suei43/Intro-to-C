5776814f44464b9bf9c22ff78363d39d4ff07aa2
Suei43 <sueiraphael@gmail.com>
2023-05-25 00:16:44 +0100

    typecasting

--- a.c
+++ a.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 
 int main(){
+    //ASCII mainpulation
     char letter = 'a';
-    printf("ASCII manipulation\n");
-    printf("%c\n", ++letter);
+    printf("a+1: %c\n", ++letter);
+
+    //Typecasting
+    double num = 10.34;
+    int num2 = (int)num;
+    printf("num: %d\n", num2);
 }
